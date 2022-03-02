e6cf237ef08336c94c0b672b642375f5df64e437
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2022-03-02 19:15:48 -0300

    refactor: ensure SignupController.handle implements correct interface

--- src/presentations/controller/signup.ts
+++ src/presentations/controller/signup.ts
@@ -1,16 +1,22 @@
+import { HttpResponse, HttpRequest } from '../../presentations/protocols/http'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: new Error('Missing param: name')
       }
     }
-    if (!httpRequest.body.mail) {
+    if (!httpRequest.body.email) {
       return {
         statusCode: 400,
         body: new Error('Missing param: email')
       }
+    }
+    return {
+      statusCode: 200,
+      body: ''
     }
   }
 }
